709aa41e97f65b869f17254df3bb22cc4c576d95
lipin <ping.li1688@gmail.com>
2013-12-25 11:51:10 +0800

    halt carts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224060737) do
+ActiveRecord::Schema.define(version: 20131225022352) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
